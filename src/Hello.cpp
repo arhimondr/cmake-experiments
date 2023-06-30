70d47e0259a0bb3b3c48d6e3a1c03bba2a16b99d
Andrii Rosa <andriyrosa@gmail.com>
2023-06-30 11:51:09 -0400

    Initial commit

---
+++ src/Hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <fmt/ranges.h>
+
+#include "static/Hello.h"
+
+void Hello::print()
+{
+    std::vector<int> v = {1, 2, 3};
+    fmt::print("{}\n", v);
+}
